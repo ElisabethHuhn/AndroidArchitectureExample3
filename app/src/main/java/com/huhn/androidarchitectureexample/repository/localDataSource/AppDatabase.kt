4095dbdbdc7329b611701b36578a949919aab7d8
Elisabeth Huhn <elisabethhuhn@gmail.com>
2023-11-15 10:27:14 -0500

    Update gradle versions Migrate to KSP

--- app/src/main/java/com/huhn/androidarchitectureexample/repository/localDataSource/AppDatabase.kt
+++ app/src/main/java/com/huhn/androidarchitectureexample/repository/localDataSource/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.huhn.androidarchitectureexample.repository.localDataSource.dbModel.DBDriver
 import com.huhn.androidarchitectureexample.repository.localDataSource.dbModel.DBRoute
 
-@Database(entities = [DBDriver::class, DBRoute::class], version = 1)
+@Database(entities = [DBDriver::class, DBRoute::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun dbDriverDao(): DBDriverDao
     abstract fun dbRouteDao(): DBRouteDao
